1492768644ccaadaab3d438f49353e64b3eed142
jktham-sf <jonas.tham@gmx.ch>
2022-07-22 21:25:10 +0200

    Add basic network

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-print("test")
+import loader
+import network
+
+training_data, validation_data, test_data = loader.load_data_wrapper()
+
+net = network.Network([784, 30, 10])
+net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
